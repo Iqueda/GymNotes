06c724f23bb73c2276fa2a0915bfe8e34aea875d
Gabriel Lobato <iqueda.gabriel@gmail.com>
2016-09-11 20:21:21 +1200

    change on UserDetails

--- app/components/UserDetails.js
+++ app/components/UserDetails.js
@@ -5,37 +5,37 @@ class UserDetails extends Component {
     render() {
         return (
             <div className='container'>
-                    <form className='form-horizontal'>
-                    <div className='col-sm-6'>
+               <form className='form-horizontal'>
+                  <div className='col-sm-6'>
                     <h1 className='text-center'>User Details</h1>
-                    <label> User Name:</label>
-                    <input type='text' className='form-control' placeholder='Tells us your Name'/>
-                    <label> Heigth:</label>
-                    <input type='text' className='form-control' placeholder='what is your heigth?'/>
-                    <label> Weigth:</label>
-                    <input type='text' className='form-control' placeholder='what is  your weigth?'/>
+                      <label> User Name:</label>
+                        <input type='text' className='form-control' placeholder='Tells us your Name'/>
+                      <label> Heigth:</label>
+                        <input type='text' className='form-control' placeholder='what is your heigth?'/>
+                      <label> Weigth:</label>
+                        <input type='text' className='form-control' placeholder='what is  your weigth?'/>
                     </div>
                     <div className='col-sm-3'>
-                    <h1 className='text-center'>Measurements</h1>
-                    <label> Chest:</label>
-                    <input type='text' className='form-control' placeholder='Chest Measure'/>
-                    <label> Shoulder:</label>
-                    <input type='text' className='form-control' placeholder='Shoulder Measure'/>
-                    <label> Legs:</label>
-                    <div>
-                    <input type='text' className='form-inline' placeholder='Rigth Leg Measure'/>
-                    <input type='text' className='form-inline' placeholder='Left Leg Measure'/>
-                    </div>
-                    <label> Bicpes:</label>
-                    <div>
-                    <input type='text' className='form-inline' placeholder='Rigth Biceps'/>
-                    <input type='text' className='form-inline' placeholder='Left Biceps'/>
-                    </div>
-                    <label> Triceps:</label>
-                    <div>
-                    <input type='text' className='form-inline' placeholder='Rigth Tricpes'/>
-                    <input type='text' className='form-inline' placeholder='Left Triceps'/>
-                    </div>
+                      <h1 className='text-center'>Measurements</h1>
+                        <label> Chest:</label>
+                          <input type='text' className='form-control' placeholder='Chest Measure'/>
+                        <label> Shoulder:</label>
+                          <input type='text' className='form-control' placeholder='Shoulder Measure'/>
+                        <label> Legs:</label>
+                     <div>
+                       <input type='text' className='form-inline' placeholder='Rigth Leg Measure'/>
+                       <input type='text' className='form-inline' placeholder='Left Leg Measure'/>
+                     </div>
+                       <label> Bicpes:</label>
+                     <div>
+                       <input type='text' className='form-inline' placeholder='Rigth Biceps'/>
+                       <input type='text' className='form-inline' placeholder='Left Biceps'/>
+                     </div>
+                       <label> Triceps:</label>
+                     <div>
+                       <input type='text' className='form-inline' placeholder='Rigth Tricpes'/>
+                       <input type='text' className='form-inline' placeholder='Left Triceps'/>
+                      </div>
                     </div>
                   </form>  
                   <div className='col-sm-12'>
